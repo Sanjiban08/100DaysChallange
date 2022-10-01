ee79a6ed811605a63ace7d2703fed5813f834f9b
Sanjiban08 <sanjibandon555@gmail.com>
2022-10-01 19:54:52 +0530

    Day 1

---
+++ Day1/D1.cpp
@@ -0,0 +1,24 @@
+//CHECKING THE SIZES OF THE VARIABELS--
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "Size of integer: " << sizeof(a) << endl;
+	float b;
+	cout << "Size of float: " << sizeof(b) << endl;
+	char c;
+	cout << "Size of character: " << sizeof(c) << endl;
+	bool d;
+	cout << "Size of boolean: " << sizeof(d) << endl;
+	short int si;
+	cout << "Size of short integer: " << sizeof(si) << endl;
+	long int li;
+	cout << "Size of long integer: " << sizeof(li) << endl;
+	return 0;
+}
+
+
+
+
