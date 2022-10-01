2dbe2ef0d69a1aef12d01877a2788c5570ad6fc3
Sanjiban08 <sanjibandon555@gmail.com>
2022-10-01 12:54:51 +0530

    Day1

--- Day1/sum.cpp
+++ Day1/sum.cpp
@@ -1,1 +1,19 @@
-jhgjgckugil
+#include <iostream>
+using namespace std;
+
+int addTwoNumber(int A, int B)
+{
+	return A + B;
+}
+int main()
+{
+	int A, B;
+    cout << "Enter two numbers to add: \n";
+    cin >> A;
+    cin >> B;
+	cout << "sum = " << addTwoNumber(A, B);
+	return 0;
+}
+
+
+
