b55d60aaa276a3647996195424232cbc6b960419
Sanjiban08 <sanjibandon555@gmail.com>
2022-10-07 16:12:18 +0530

    Conditional Statements Day 1 of 5(Grading according to Marks)

--- Day_7(Conditional Statement)/conditionalstatement1.cpp
+++ Day_7(Conditional Statement)/conditionalstatement1.cpp
@@ -1,4 +1,4 @@
-//A school has following rules for grading system: a. Below 25 - F, b. 25 to 45 - E, c. 45 to 50 - D
+//A school has following rules for grading system: a. Below 25 - F, b. 25 to 45 - E, c. 45 to 50 - D,
 //d. 50 to 60 - C ,e. 60 to 80 - B, f. Above 80 - A. Ask user to enter marks and print the corresponding grade.
 
 #include <iostream>
