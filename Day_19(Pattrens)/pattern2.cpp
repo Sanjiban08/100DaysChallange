91013737675c5b4852d36bb327d908f744bcd44e
Sanjiban08 <sanjibandon555@gmail.com>
2022-10-19 08:51:07 +0530

    Making Patterns Day 2.

---
+++ Day_19(Pattrens)/pattern2.cpp
@@ -0,0 +1,16 @@
+// Print Inverted Half Pyramid using numbers ----
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = n; i >= 1; --i) {
+		for(int j = 1; j <= i; ++j) {
+			cout << j << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
