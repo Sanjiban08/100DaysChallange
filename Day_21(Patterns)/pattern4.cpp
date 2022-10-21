d993eaaef820457d1fa9a0324a17002c98b2edc2
Sanjiban08 <sanjibandon555@gmail.com>
2022-10-21 09:59:40 +0530

    Making Patterns Day 4.

---
+++ Day_21(Patterns)/pattern4.cpp
@@ -0,0 +1,19 @@
+// Print 0-1 pattern ----
+
+#include <iostream>
+
+int main() {
+	int i,j,r;
+    using namespace std;
+	cin>>r;
+	for (i = 1; i <= r; i++) {
+		for (j=1;j<=i;j++) {
+			if ((i+j)%2==0)
+				cout<<" 1";
+			else
+				cout<<" 0";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
