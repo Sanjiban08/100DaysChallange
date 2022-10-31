e15bc5e54039e533a5afc4cef196327287d16304
Sanjiban08 <sanjibandon555@gmail.com>
2022-10-31 16:03:54 +0530

    Binary Number System Day 4.

---
+++ Day_30(Binary Number System)/bns4.cpp
@@ -0,0 +1,16 @@
+// Reversing a Number. ----
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int rev_num=0;
+    while(n>0) {
+        int rem=n%10;
+        rev_num=rev_num*10+rem;
+        n=n/10;
+    }
+    cout<<rev_num<<"\n";
+    return 0;
+}
