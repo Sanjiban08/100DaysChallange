7bddb7d42ed6213c70b78f40cec86c361b0e753e
Sanjiban08 <sanjibandon555@gmail.com>
2022-11-04 21:20:07 +0530

    Using Functions Day 4.

---
+++ Day_34(Functions)/function4.cpp
@@ -0,0 +1,13 @@
+// Find the Square Root of a Number. ----  
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double number, squareRoot;
+    number = 25.0;
+    squareRoot = sqrt(number);
+    cout << "Square root of " << number << " = " << squareRoot;
+    return 0;
+}
