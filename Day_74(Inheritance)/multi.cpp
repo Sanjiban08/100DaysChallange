ee716fa1d5311bf369a5f01318f28af97f5f7d89
Sanjiban08 <sanjibandon555@gmail.com>
2022-12-14 18:11:11 +0530

    Inheritance Day 1.

---
+++ Day_74(Inheritance)/multi.cpp
@@ -0,0 +1,17 @@
+// Implement Miltilevel Inheritance. ----
+
+#include <iostream>
+using namespace std;
+class A {
+    public:
+      void display() {
+          cout<<"Base class content.";
+      }
+};
+class B : public A {};
+class C : public B {};
+int main() {
+    C obj;
+    obj.display();
+    return 0;
+}
