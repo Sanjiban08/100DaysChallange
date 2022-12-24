31e8821eae00233ca0f04ec6fb757f5aa46ebcb1
Sanjiban08 <sanjibandon555@gmail.com>
2022-12-24 09:15:24 +0530

    Implementing Stack Day 1.

---
+++ Day_84(Stack)/stack1.cpp
@@ -0,0 +1,25 @@
+// Remove elements from a Stack. ----
+
+#include <iostream>
+#include <stack>
+using namespace std;
+void display_stack(stack<string> st);
+int main() {
+    stack<string> colors;
+    colors.push("Red");
+    colors.push("Orange");
+    colors.push("Blue");
+    cout << "Initial Stack: ";
+    display_stack(colors);
+    colors.pop();
+    cout << "Final Stack: ";
+    display_stack(colors);
+    return 0;
+}
+void display_stack(stack<string> st) {
+    while(!st.empty()) {
+        cout << st.top() << ", ";
+        st.pop();
+    }
+    cout << endl;
+}
