a8a79a5e47a71fcd32c956f193dccd984edbf579
Sanjiban08 <sanjibandon555@gmail.com>
2022-12-28 23:28:39 +0530

    Implementing Stack Day 5.

---
+++ Day_88(Stack)/stack5.cpp
@@ -0,0 +1,32 @@
+// C++ program to check for balanced brackets. ----
+
+#include <bits/stdc++.h>
+using namespace std;
+bool areBracketsBalanced(string expr) {
+	stack<char> temp;
+	for (int i = 0; i < expr.length(); i++) {
+		if (temp.empty()) {
+			temp.push(expr[i]);
+		}
+		else if ((temp.top() == '(' && expr[i] == ')') || (temp.top() == '{' && expr[i] == '}') || (temp.top() == '[' && expr[i] == ']')) {
+			temp.pop();
+		}
+		else {
+			temp.push(expr[i]);
+		}
+	}
+	if (temp.empty()) {
+		return true;
+	}
+	return false;
+}
+int main() {
+	string expr = "{()}[]";
+
+	// Function call
+	if (areBracketsBalanced(expr))
+		cout << "Balanced";
+	else
+		cout << "Not Balanced";
+	return 0;
+}
